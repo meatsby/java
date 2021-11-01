0ba0b9eefeb500ba734bd53f5bbe81e35d0f0d8e
meatsby <meatsby@gmail.com>
2021-11-01 16:54:53 +0900

    Initial commit

---
+++ src/Number.java
@@ -0,0 +1,24 @@
+public class Number {
+    public static void main(String[] args) {
+        byte byteInteger = 10;
+        short shortInteger = 20;
+        int intInteger = 30;
+        long longInteger = 40;
+
+        int sum = byteInteger + shortInteger;
+        int sum2 = shortInteger + intInteger;
+        long sum3 = intInteger + longInteger;
+        long sum4 = byteInteger + longInteger;
+
+        System.out.println(byteInteger);
+        System.out.println(shortInteger);
+        System.out.println(intInteger);
+        System.out.println(longInteger);
+
+        float floatNumber = 1.2f;
+        double doubleNumber = 4.3;
+
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+    }
+}
---
+++ src/Operator.java
@@ -0,0 +1,19 @@
+public class Operator {
+    public static void main(String[] args) {
+        System.out.println(2 + 3);
+        System.out.println(5 - 2);
+        System.out.println(2 * 3);
+        System.out.println(6 / 3);
+        System.out.println(7 % 3);
+
+        int number = 5;
+        number = (int) 1.2;
+        System.out.println(number);
+
+        System.out.println(1 + 1.2);
+        System.out.println(7 / 2);
+        System.out.println(7 / 2.0);
+
+        System.out.println(2 < 3);
+    }
+}
