ea0f4d8bee69b0fd77371e834a1348940e3bd109
meatsby <meatsby@gmail.com>
2021-11-09 19:00:59 +0900

    21.11.09

---
+++ src/MemberVariable.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MemberVariable {
+
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        echoSomeInputs();
+    }
+
+    private static void echoSomeInputs() {
+        echoInteger();
+        echoString();
+    }
+
+    private static void echoInteger() {
+        int input = s.nextInt();
+        System.out.println(input);
+    }
+
+    private static void echoString() {
+        String input = s.nextLine();
+        System.out.println(input);
+    }
+}
