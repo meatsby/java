55c26cc3cfb5532a7b71a484b91be2ede9a290f0
meatsby <meatsby@gmail.com>
2021-11-10 12:16:09 +0900

    21.11.10

--- src/ListExample03.java
+++ src/ListExample03.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ListExample03 {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("앙", "앙");
+        List<String> names2 = new ArrayList<>();
+        names2.add("김건부");
+        names2.add("허수");
+        names2.add("장용준");
+        names2.add("이현우");
+
+        System.out.println(names);
+        System.out.println(names2);
+    }
 }
---
+++ src/SetExample.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+    public static void main(String[] args) {
+        Set<String> myFav = new HashSet<>();
+        myFav.add("Chicken");
+        myFav.add("Chicken");
+        myFav.add("Pizza");
+
+        myFav.remove("Chicken");
+
+        System.out.println(myFav);
+    }
+}
