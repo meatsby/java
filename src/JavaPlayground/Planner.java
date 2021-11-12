7e4e75b542dcef61cae7b1bb79240bd282f2c1ae
meatsby <meatsby@gmail.com>
2021-11-12 19:43:00 +0900

    21.11.12

--- src/JavaPlayground/Planner.java
+++ src/JavaPlayground/Planner.java
@@ -1,10 +1,12 @@
 package JavaPlayground;
 
+import java.io.*;
 import java.util.*;
 
 public class Planner {
     public Scanner s = new Scanner(System.in);
     public HashMap<String, String> planner = new HashMap<>();
+    private static final String SAVE_FILE = "calendar.dat";
 
     public void registerCmd() {
         System.out.println("[일정 등록] 날짜를 입력하세요.");
@@ -15,13 +17,37 @@ public class Planner {
         System.out.print("> ");
         String plan = s.next();
 
-        ArrayList<String> planList = new ArrayList<>();
-        planList.add(plan);
         planner.put(date, plan);
         System.out.println("일정이 등록되었습니다.");
+
+        File f = new File(SAVE_FILE);
+        try {
+            FileWriter fw = new FileWriter(f, true);
+            fw.write(date + "," + plan + "\n");
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void searchCmd() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(SAVE_FILE));
+            while (true) {
+                String line = null;
+                try {
+                    line = br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (line == null) break;
+                String date = line.split(",")[0];
+                String detail = line.split(",")[1];
+                planner.put(date, detail);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         System.out.println("[일정 검색] 날짜를 입력하세요.");
         System.out.print("> ");
         String date = s.next();
