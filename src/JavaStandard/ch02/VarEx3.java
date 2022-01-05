c8ad57e18a1232c1c0216fb31bdc714a8d472749
meatsby <meatsby@gmail.com>
2022-01-05 13:51:51 +0900

    feat:ex3

---
+++ src/JavaStandard/ch02/VarEx3.java
@@ -0,0 +1,10 @@
+package JavaStandard.ch02;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        final int score = 100;
+        // score = 200;
+
+        System.out.println(score);
+    }
+}
