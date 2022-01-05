f8b243cd3ef84e007f1f091b7de3bce36f9fc000
meatsby <meatsby@gmail.com>
2022-01-05 14:24:59 +0900

    feat:ex3

--- src/JavaStandard/ch02/VarEx3.java
+++ src/JavaStandard/ch02/VarEx3.java
@@ -15,9 +15,16 @@ public class VarEx3 {
 
         float f = 3.14f;
         double d = 3.14f;
-        System.out.println(10.);
-        System.out.println(.10);
-        System.out.println(10f);
-        System.out.println(1e3);
+
+        char ch = 'A';
+        int i = 'A';
+
+        String str = "";
+        String str2 = "ABCD";
+        String str3 = "123";
+        String str4 = str2 + str3;
+        System.out.println(str4);
+        System.out.println(""+7+7);
+        System.out.println(7+7+"");
     }
 }
