452cc089e811b61eee37127971eefa47015f55bd
meatsby <meatsby@gmail.com>
2022-01-05 13:18:57 +0900

    feat:example2_1

--- src/JavaStandard/ch01/Ex2_1.java
+++ src/JavaStandard/ch01/Ex2_1.java
@@ -2,6 +2,20 @@ package JavaStandard.ch01;
 
 public class Ex2_1 {
 	public static void main(String[] args) {
+		// println 줄바꿈
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		// print 줄바꿈 안함
+		System.out.print("Hello");
+		System.out.print("Hello");
+		System.out.print("Hello");
+		// 문자열을 출력함
+		System.out.println("3+5");
+		// 쌍따옴표를 제외하고 계산 후 출력
+		System.out.println(5+3);
+		System.out.println(5-3);
+		System.out.println(5*3);
+		System.out.println(5/3);
 	}
 }
