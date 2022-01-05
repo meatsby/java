a2520439c465f4e93f3309e94824c272827a21a3
meatsby <meatsby@gmail.com>
2022-01-05 14:13:45 +0900

    feat:ex3

--- src/JavaStandard/ch02/VarEx3.java
+++ src/JavaStandard/ch02/VarEx3.java
@@ -4,7 +4,20 @@ public class VarEx3 {
     public static void main(String[] args) {
         final int score = 100;
         // score = 200;
+        boolean power = true;
 
-        System.out.println(score);
+        byte b = 127;
+
+        int oct = 010;
+        int hex = 0x10;
+
+        long l = 10_000_000_000L;
+
+        float f = 3.14f;
+        double d = 3.14f;
+        System.out.println(10.);
+        System.out.println(.10);
+        System.out.println(10f);
+        System.out.println(1e3);
     }
 }
