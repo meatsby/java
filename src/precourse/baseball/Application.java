8b16b3d7ecc046d02b4b7636bf79d54fc6f52c8b
meatsby <meatsby@gmail.com>
2022-01-05 13:04:00 +0900

    refactor:delete precourse

--- src/precourse/baseball/Application.java
+++
@@ -1,9 +0,0 @@
-package precourse.baseball;
-
-public class Application {
-
-   public static void main(String[] args) {
-       Game game = new Game(new Computer(), new Validation());
-       game.startGame();
-   }
-}
