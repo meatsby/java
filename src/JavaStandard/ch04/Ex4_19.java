e86e7865acab39c1342dc276368d3d52ca13e681
meatsby <meatsby@gmail.com>
2022-01-13 14:27:08 +0900

    feat:Ex4_19

---
+++ src/JavaStandard/ch04/Ex4_19.java
@@ -0,0 +1,15 @@
+package JavaStandard.ch04;
+
+public class Ex4_19 {
+	public static void main(String[] args) {
+		Loop1 : for (int i = 2; i <= 9; i++) {
+			for (int j =1; j <= 9; j++) {
+				if (j == 5) {
+					break Loop1;
+				}
+				System.out.printf("%d*%d=%d%n", i, j, i*j);
+			}
+			System.out.println();
+		}
+	}
+}
