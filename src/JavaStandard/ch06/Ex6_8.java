ae69749f3527a05871e85325fbeff4fd9157a2ee
meatsby <meatsby@gmail.com>
2022-01-15 15:01:51 +0900

    feat:Ex6_8

---
+++ src/JavaStandard/ch06/Ex6_8.java
@@ -0,0 +1,22 @@
+package JavaStandard.ch06;
+
+class Data3 {
+	int x;
+}
+
+public class Ex6_8 {
+	public static void main(String[] args) {
+		Data3 d = new Data3();
+		d.x = 10;
+
+		Data3 d2 = copy(d);
+		System.out.println("d.x  = " + d.x);
+		System.out.println("d2.x = " + d2.x);
+	}
+
+	static Data3 copy(Data3 d) {
+		Data3 temp = new Data3();
+		temp.x = d.x;
+		return temp;
+	}
+}
