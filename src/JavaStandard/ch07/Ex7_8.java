5bb30ef9942b42e8b432d37e04cb3b4dbd3a6e12
meatsby <meatsby@gmail.com>
2022-01-20 18:16:03 +0900

    feat:Ex7_8

---
+++ src/JavaStandard/ch07/Ex7_8.java
@@ -0,0 +1,61 @@
+package JavaStandard.ch07;
+
+class Product {
+	int price;
+	int bonusPoint;
+
+	Product(int price) {
+		this.price = price;
+		bonusPoint = (int)(price/10.0);
+	}
+}
+
+class Tv1 extends Product {
+	Tv1() {
+		// 조상타입 Product 의 생성자를 불러옴
+		super(100);
+	}
+
+	// Object 클래스의 toString() 오버라이딩
+	@Override
+	public String toString() {
+		return "Tv";
+	}
+}
+
+class Computer extends Product {
+	Computer() {
+		super(200);
+	}
+
+	public String toString() {
+		return "Computer";
+	}
+}
+
+class Buyer {
+	int money = 1000;
+	int bonusPoint = 0;
+
+	void buy(Product p) {
+		if (money < p.price) {
+			System.out.println("잔액이 부족합니다.");
+		}
+
+		money -= p.price;
+		bonusPoint += p.bonusPoint;
+		System.out.println(p + " 을/를 구입하셨습니다.");
+	}
+}
+
+public class Ex7_8 {
+	public static void main(String[] args) {
+		Buyer b = new Buyer();
+
+		b.buy(new Tv1());
+		b.buy(new Computer());
+
+		System.out.println("남은 돈 : " + b.money + "만원");
+		System.out.println("보너스점수 : " + b.bonusPoint + "점");
+	}
+}
