46760ae0c9d0eaf6ea2d5684bee9315a69c469a6
meatsby <meatsby@gmail.com>
2022-01-22 17:57:07 +0900

    feat:ExceptionTest

---
+++ src/JavaStandard/ch08/ExceptionTest.java
@@ -0,0 +1,11 @@
+package JavaStandard.ch08;
+
+public class ExceptionTest {
+	public static void main(String[] args) {
+		// 컴파일 에러
+		// system.out.println(args[0]);
+
+		// 런타임 에러
+		System.out.println(args[0]);
+	}
+}
