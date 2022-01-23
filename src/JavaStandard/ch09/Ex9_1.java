b8fa0384298440b26868c4d430e411362e696022
meatsby <meatsby@gmail.com>
2022-01-23 13:44:10 +0900

    feat:Ex9_1

---
+++ src/JavaStandard/ch09/Ex9_1.java
@@ -0,0 +1,29 @@
+package JavaStandard.ch09;
+
+class Value {
+	int value;
+
+	Value(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Value)) {
+			return false;
+		}
+
+		Value v = (Value)obj;
+
+		return this.value == v.value;
+	}
+}
+
+public class Ex9_1 {
+	public static void main(String[] args) {
+		Value v1 = new Value(10);
+		Value v2 = new Value(10);
+
+		System.out.println(v1.equals(v2));
+	}
+}
