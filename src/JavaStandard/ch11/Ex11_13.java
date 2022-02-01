a748383894714668a42b9ef2de9f2c6c930b34da
meatsby <meatsby@gmail.com>
2022-02-01 21:40:57 +0900

    feat:Ex11_13

---
+++ src/JavaStandard/ch11/Ex11_13.java
@@ -0,0 +1,17 @@
+package JavaStandard.ch11;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Ex11_13 {
+	public static void main(String[] args) {
+		Set set = new TreeSet(); // 정렬 필요 없음
+
+		for (int i = 0; set.size() < 6; i++) {
+			int num = (int)(Math.random()*45) + 1;
+			set.add(num);
+		}
+
+		System.out.println(set);
+	}
+}
