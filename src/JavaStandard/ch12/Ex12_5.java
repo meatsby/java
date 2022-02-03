f16354042eeab4f44a34473e8bd3fb910b8b0b8d
meatsby <meatsby@gmail.com>
2022-02-03 22:56:33 +0900

    feat:Ex12_5

---
+++ src/JavaStandard/ch12/Ex12_5.java
@@ -0,0 +1,42 @@
+package JavaStandard.ch12;
+
+enum Direction { EAST, SOUTH, WEST, NORTH }
+
+public class Ex12_5 {
+	public static void main(String[] args) {
+		Direction d1 = Direction.EAST;
+		Direction d2 = Direction.valueOf("WEST");
+		Direction d3 = Enum.valueOf(Direction.class, "EAST");
+
+		System.out.println("d1 = " + d1);
+		System.out.println("d2 = " + d2);
+		System.out.println("d3 = " + d3);
+
+		System.out.println("d1 == d2 ? " + (d1 == d2));
+		System.out.println("d1 == d3 ? " + (d1 == d3));
+		System.out.println("d1.equals(d3) ? " + d1.equals(d3));
+		System.out.println("d1.compareTo(d3) ? " + d1.compareTo(d3));
+		System.out.println("d1.compareTo(d2) ? " + d1.compareTo(d2));
+
+		switch (d1) {
+			case EAST:
+				System.out.println("EAST");
+				break;
+			case SOUTH:
+				System.out.println("SOUTH");
+				break;
+			case WEST:
+				System.out.println("WEST");
+				break;
+			case NORTH:
+				System.out.println("NORTH");
+				break;
+		}
+
+		Direction[] dArr = Direction.values();
+
+		for (Direction d : dArr) {
+			System.out.println(d.name() + " = " + d.ordinal());
+		}
+	}
+}
