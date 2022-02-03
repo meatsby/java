50ce415d33e55dc40e2c74e0473bd0811d14dcd2
meatsby <meatsby@gmail.com>
2022-02-04 04:41:44 +0900

    feat:Ex12_7

--- src/JavaStandard/ch12/Ex12_7.java
+++ src/JavaStandard/ch12/Ex12_7.java
@@ -10,6 +10,12 @@ class Child extends Parent {
 	void parentMethod() {}
 }
 
+@FunctionalInterface // 함수형 인터페이스는 하나의 추상메서드만 가능
+interface Testable {
+	void test();  // 추상메서드
+	// void check(); // 추상메서드
+}
+
 public class Ex12_7 {
 	public static void main(String[] args) {
 		Child c = new Child();
