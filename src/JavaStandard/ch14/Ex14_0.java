6dd857f7c7e0b91aa208008c3029decf42615a38
meatsby <meatsby@gmail.com>
2022-02-06 05:30:32 +0900

    feat:Ex14_0

---
+++ src/JavaStandard/ch14/Ex14_0.java
@@ -0,0 +1,14 @@
+package JavaStandard.ch14;
+
+public class Ex14_0 {
+	public static void main(String[] args) {
+		// Object obj = (a, b) -> a > b ? a : b; // 람다식. 익명객체
+		Object obj = new Object() {
+			int max(int a, int b) {
+				return a > b ? a : b;
+			}
+		};
+
+		// int value = obj.max(3, 5); // 함수형 인터페이스스
+	}
+}
