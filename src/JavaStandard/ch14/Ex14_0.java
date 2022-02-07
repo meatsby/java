9536e43db12289cfb25a3ab4e8f39e99cde18fa8
meatsby <meatsby@gmail.com>
2022-02-07 11:56:58 +0900

    feat:Ex14_0

--- src/JavaStandard/ch14/Ex14_0.java
+++ src/JavaStandard/ch14/Ex14_0.java
@@ -3,12 +3,22 @@ package JavaStandard.ch14;
 public class Ex14_0 {
 	public static void main(String[] args) {
 		// Object obj = (a, b) -> a > b ? a : b; // 람다식. 익명객체
-		Object obj = new Object() {
-			int max(int a, int b) {
-				return a > b ? a : b;
-			}
-		};
+		// MyFunction f = new MyFunction() {
+		// 	public int max(int a, int b) {
+		// 		return a > b ? a : b;
+		// 	}
+		// };
 
-		// int value = obj.max(3, 5); // 함수형 인터페이스스
+		// 람다식을 다루기 위한 참조변수의 타입은 함수형 인터페이스로 한다.
+		MyFunction f = (a, b) -> a > b ? a : b; // 람다식. 익명객체
+
+		int value = f.max(3, 5); // 함수형 인터페이스
+		System.out.println("value = " + value);
 	}
+}
+
+@FunctionalInterface
+interface MyFunction {
+	// public abstract int max(int a, int b);
+	int max(int a, int b);
 }
