ddeb434231e6b0938e6bcab30eeb9a8c47f0aabb
meatsby <meatsby@gmail.com>
2022-03-12 10:00:00 +0000

    feat: Ex2_4

---
+++ practice/src/main/java/JavaStandard/ch02/Ex2_4.java
@@ -0,0 +1,15 @@
+package JavaStandard.ch02;
+
+public class Ex2_4 {
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 2;
+        char c = 'A';
+
+        int finger = 10;
+        long big = 100_000_000_000L;
+        long hex = 0xFFFF_FFFF_FFFF_FFFFL;
+
+        System.out.printf("finder=[%5d]%n", finger);
+    }
+}
