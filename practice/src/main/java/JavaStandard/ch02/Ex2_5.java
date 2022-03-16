8b1d48444bc3855b8886aa5600c4a48911111272
meatsby <meatsby@gmail.com>
2022-03-16 10:00:00 +0000

    feat: Ex2_5

---
+++ practice/src/main/java/JavaStandard/ch02/Ex2_5.java
@@ -0,0 +1,13 @@
+package JavaStandard.ch02;
+
+public class Ex2_5 {
+    public static void main(String[] args) {
+        String url = "www.codechobo.com";
+
+        float pi = 3.14e3f;
+
+        System.out.printf("%f, %e, %g%n", pi, pi, pi);
+
+        System.out.printf("[%-20s]%n", url);
+    }
+}
