aa68e3a22bddeeb8535c8376bad11dee77288cc1
meatsby <meatsby@gmail.com>
2022-03-20 10:00:00 +0000

    feat: Ex2_6

---
+++ practice/src/main/java/JavaStandard/ch02/Ex2_6.java
@@ -0,0 +1,15 @@
+package JavaStandard.ch02;
+
+import java.util.Scanner;
+
+public class Ex2_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int num = Integer.parseInt(input);
+
+        System.out.println(input);
+        System.out.println(num);
+    }
+}
