1464e8c0e67b316437de872c60d33cd21a172363
meatsby <meatsby@gmail.com>
2022-03-26 10:00:00 +0000

    feat: Ex2_7

---
+++ practice/src/main/java/JavaStandard/ch02/Ex2_7.java
@@ -0,0 +1,9 @@
+package JavaStandard.ch02;
+
+public class Ex2_7 {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        System.out.printf("%c=%d(%#X)%n", ch, (int)ch, (int)ch);
+    }
+}
