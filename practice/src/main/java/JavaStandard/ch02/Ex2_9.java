b1eac62b69d7d6d2dd136ccdcdf038203fa0fc35
meatsby <meatsby@gmail.com>
2022-04-03 10:00:00 +0000

    feat: Ex2_9

---
+++ practice/src/main/java/JavaStandard/ch02/Ex2_9.java
@@ -0,0 +1,15 @@
+package JavaStandard.ch02;
+
+public class Ex2_9 {
+    public static void main(String[] args) {
+        short sMin = -32768;
+        short sMax = 32767;
+        char cMin = 0;
+        char cMax = 65535;
+
+        System.out.println(sMin);
+        System.out.println(sMax);
+        System.out.println(cMin);
+        System.out.println(cMax);
+    }
+}
