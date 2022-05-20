453f386ee77d7e1eb3564b23919815bfe7e041ff
meatsby <meatsby@gmail.com>
2022-05-20 10:00:00 +0000

    feat: Ex3_12

---
+++ practice/src/main/java/JavaStandard/ch03/Ex3_12.java
@@ -0,0 +1,11 @@
+package JavaStandard.ch03;
+
+public class Ex3_12 {
+	public static void main(String args[]) { 
+		int x = 10;
+		int y = 8;
+
+		System.out.printf("%d %d, %n", x, y);
+		System.out.printf("%d, %d %n", x / y, x % y);
+	}
+}
