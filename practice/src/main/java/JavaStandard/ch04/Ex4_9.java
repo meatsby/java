e79c16c4b3c0d5bef9f03c840a6ed4a9a26e9032
meatsby <meatsby@gmail.com>
2022-06-17 10:00:00 +0000

    feat: Ex4_9

---
+++ practice/src/main/java/JavaStandard/ch04/Ex4_9.java
@@ -0,0 +1,13 @@
+package JavaStandard.ch04;
+
+public class Ex4_9 {
+    public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.print(i);
+		}
+    }
+}
