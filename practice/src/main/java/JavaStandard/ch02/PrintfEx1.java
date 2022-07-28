2551500a6980e09c7f01343ae8958de829e45747
meatsby <meatsby@gmail.com>
2022-07-28 10:00:00 +0000

    refactor: PrintfEx1

--- practice/src/main/java/JavaStandard/ch02/PrintfEx1.java
+++ practice/src/main/java/JavaStandard/ch02/PrintfEx1.java
@@ -1,30 +1,31 @@
 package JavaStandard.ch02;
 
 public class PrintfEx1 {
-	public static void main(String[] args) {
-		System.out.println(10.0/3);
 
-		System.out.printf("%d%n", 15);
-		System.out.printf("%#o%n", 15);
-		System.out.printf("%#x%n", 15);
-		System.out.printf("%s%n", Integer.toBinaryString(15));
+    public static void main(String[] args) {
+        System.out.println(10.0 / 3);
 
-		double f = 123.456789;
-		System.out.printf("%f%n", f);
-		System.out.printf("%e%n", f);
-		System.out.printf("%g%n", f);
+        System.out.printf("%d%n", 15);
+        System.out.printf("%#o%n", 15);
+        System.out.printf("%#x%n", 15);
+        System.out.printf("%s%n", Integer.toBinaryString(15));
 
-		System.out.printf("[%5d]%n", 1234567);
-		System.out.printf("[%-5d]%n", 10);
-		System.out.printf("[%05d]%n", 10);
+        double f = 123.456789;
+        System.out.printf("%f%n", f);
+        System.out.printf("%e%n", f);
+        System.out.printf("%g%n", f);
 
-		double d = 1.23456789;
-		System.out.printf("%14.6f%n", d);
+        System.out.printf("[%5d]%n", 1234567);
+        System.out.printf("[%-5d]%n", 10);
+        System.out.printf("[%05d]%n", 10);
 
-		String url = "www.codechobo.com";
-		System.out.printf("[%s]%n", url);
-		System.out.printf("[%20s]%n", url);
-		System.out.printf("[%-20s]%n", url);
-		System.out.printf("[%.10s]%n", url);
-	}
+        double d = 1.23456789;
+        System.out.printf("%14.6f%n", d);
+
+        String url = "www.codechobo.com";
+        System.out.printf("[%s]%n", url);
+        System.out.printf("[%20s]%n", url);
+        System.out.printf("[%-20s]%n", url);
+        System.out.printf("[%.10s]%n", url);
+    }
 }
