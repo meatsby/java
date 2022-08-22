e97ae3a8bd47ceb318a7190fbca2a4c0cc11ca37
meatsby <meatsby@gmail.com>
2022-08-22 10:00:00 +0000

    refactor: ScanfEx1

--- practice/src/main/java/JavaStandard/ch02/ScanfEx1.java
+++ practice/src/main/java/JavaStandard/ch02/ScanfEx1.java
@@ -3,17 +3,13 @@ package JavaStandard.ch02;
 import java.util.Scanner;
 
 public class ScanfEx1 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 
-		// int num = scanner.nextInt();
-		// int num2 = scanner.nextInt();
-		// System.out.println(num);
-		// System.out.println(num2);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-		String input = scanner.nextLine();
-		int num = Integer.parseInt(input);
+        String input = scanner.nextLine();
+        int num = Integer.parseInt(input);
 
-		System.out.println(num);
-	}
+        System.out.println(num);
+    }
 }
