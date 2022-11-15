dbd2966e699924fc608c2976ef567ceeb426a9c1
meatsby <meatsby@gmail.com>
2022-11-15 17:32:10 +0900

    test: newScheduledThreadPoolTest

--- thread/src/test/java/ThreadTest.java
+++ thread/src/test/java/ThreadTest.java
@@ -1,21 +1,29 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ThreadTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ThreadTest.class);
 
     @Test
     void executorTest() {
         Executor executor = Executors.newSingleThreadExecutor();
-        executor.execute(() -> System.out.println("Hello World"));
+        executor.execute(() -> log.info("Hello World"));
     }
 
     @Test
@@ -59,6 +67,28 @@ public class ThreadTest {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.submit(() -> counter.set(1));
         executor.submit(() -> counter.compareAndSet(1, 2));
+    }
+
+    @Test
+    void scheduleTest() {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
+        executor.schedule(() -> {
+            log.info("Hello World");
+        }, 500, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    void scheduleAtFixedRateTest() throws InterruptedException {
+        CountDownLatch lock = new CountDownLatch(3);
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
+        ScheduledFuture<?> future = executor.scheduleAtFixedRate(() -> {
+            log.info("Hello World");
+            lock.countDown();
+        }, 500, 100, TimeUnit.MILLISECONDS);
+
+        lock.await(1000, TimeUnit.MILLISECONDS);
+        future.cancel(true);
     }
 
     private Runnable sleepThread() {
