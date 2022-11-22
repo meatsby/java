93f5dc5e8daff32d5496aee4e56e6e6cebe4b9a7
meatsby <meatsby@gmail.com>
2022-11-22 10:00:00 +0000

    feat: Ex10_10

---
+++ practice/src/main/java/JavaStandard/ch10/Ex10_10.java
@@ -0,0 +1,34 @@
+package JavaStandard.ch10;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Ex10_10 {
+    public static void main(String[] args) {
+        String pattern = "yyyy/MM/dd";
+        DateFormat df = new SimpleDateFormat(pattern);
+        Scanner s = new Scanner(System.in);
+
+        Date inDate = null;
+
+        System.out.println(pattern);
+        while (s.hasNextLine()) {
+            try {
+                inDate = df.parse(s.nextLine());
+                break;
+            } catch (Exception e) {
+                System.out.println(pattern);
+            }
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(inDate);
+        Calendar today = Calendar.getInstance();
+        long day = (cal.getTimeInMillis()
+                - today.getTimeInMillis()) / (60 * 60 * 1000);
+        System.out.println(day);
+    }
+}
