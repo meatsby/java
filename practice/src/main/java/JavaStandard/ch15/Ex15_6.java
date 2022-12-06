e48e1b389217b53afdf5fa727a2b117b344e3096
meatsby <meatsby@gmail.com>
2022-12-06 10:00:00 +0000

    feat: Ex15_6

---
+++ practice/src/main/java/JavaStandard/ch15/Ex15_6.java
@@ -0,0 +1,21 @@
+package JavaStandard.ch15;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Ex15_6 {
+    public static void main(String args[]) {
+        try {
+            FileOutputStream fos = new FileOutputStream("123.txt");
+            BufferedOutputStream bos = new BufferedOutputStream(fos, 5);
+            for (int i = '1'; i <= '9'; i++) {
+                bos.write(i);
+            }
+
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
