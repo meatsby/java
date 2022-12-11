32bb63ba7288a316d2071d54b14e1fd653fc5d03
meatsby <meatsby@gmail.com>
2022-12-11 10:00:00 +0000

    feat: Ex15_11

---
+++ practice/src/main/java/JavaStandard/ch15/Ex15_11.java
@@ -0,0 +1,24 @@
+package JavaStandard.ch15;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ex15_11 {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("Ex15_11.java");
+            BufferedReader br = new BufferedReader(fr);
+
+            String line = "";
+            for (int i = 1; (line = br.readLine()) != null; i++) {
+				if (line.indexOf(";") != -1) {
+					System.out.println(i + ":" + line);
+				}
+            }
+
+            br.close();
+        } catch (IOException e) {
+        }
+    }
+}
