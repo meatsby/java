e5a2460033a7d394edbee235d7b2ca6d2e516bd2
meatsby <meatsby@gmail.com>
2022-12-16 10:00:00 +0000

    feat: Ex15_13

---
+++ practice/src/main/java/JavaStandard/ch15/Ex15_13.java
@@ -0,0 +1,8 @@
+package JavaStandard.ch15;
+
+public class Ex15_13 {
+    public static void main(String[] args) {
+        System.out.println("out : Hello World!");
+        System.err.println("err : Hello World!");
+    }
+}
